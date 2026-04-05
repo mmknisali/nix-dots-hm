ab632beac6ee39e0cafa68dc0472a9e545e9ca81
mmknisali <alinader5525@gmail.com>
2026-04-05 13:36:29 +0300

    fixed waybar not working

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
   #importing config files
   home.file.".config/hypr".source = ./config/hypr;
   home.file.".config/kitty".source = ./config/kitty;
-  home.file.".cofing/nvim".source = ./config/nvim;
+  home.file.".config/nvim".source = ./config/nvim;
   home.file.".config/rofi".source = ./config/rofi;
   home.file.".config/starship".source = ./config/starship;
   home.file.".config/uwsm".source = ./config/uwsm;
